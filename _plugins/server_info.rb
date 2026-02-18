556353b7e45ddf37f416675d6983f76a0bcf98be
Mazin Musleh <mazin.musleh@gmail.com>
2026-02-19 01:59:57 +0300

    Simplify server_info plugin, fix pagination margin, add sideInfo z-index
    Rewrite server_info.rb to be more concise and replace 0.0.0.0 with
    localhost in Jekyll log output. Fix pagination nav using margin instead
    of margin-top. Add z-index to sideInfo on desktop for proper stacking.

--- _plugins/server_info.rb
+++ _plugins/server_info.rb
@@ -1,70 +1,51 @@
 # frozen_string_literal: true
 
+# Display local and network URLs after first serve build.
+# Replaces 0.0.0.0 with localhost in Jekyll's "Server address" log line.
+
 require 'socket'
 
-module Jekyll
-  module ServerInfo
-    class << self
-      def get_local_ip
-        # Get local IP address
-        ip = nil
-
-        begin
-          # Try to get IP by connecting to a remote address (doesn't actually connect)
-          Socket.ip_address_list.detect do |addr|
-            addr.ipv4? && !addr.ipv4_loopback? && !addr.ipv4_multicast?
-          end&.ip_address
-        rescue StandardError
-          nil
-        end
-      end
-
-      def display_network_info(site)
-        # Only show on first build when serving
-        return unless defined?(Jekyll::Commands::Serve)
-        return if @info_displayed
-
-        @info_displayed = true
-
-        # Get server configuration
-        port = site.config['port'] || 4000
-        host = site.config['host'] || 'localhost'
-        local_ip = get_local_ip
-
-        # Display network information
-        Jekyll.logger.info ""
-        Jekyll.logger.info "=" * 60
-        Jekyll.logger.info "Network Server Information".center(60)
-        Jekyll.logger.info "=" * 60
-        Jekyll.logger.info ""
-        Jekyll.logger.info "  Local Access:"
-        Jekyll.logger.info "    http://localhost:#{port}"
-        Jekyll.logger.info "    http://127.0.0.1:#{port}"
-        Jekyll.logger.info ""
-
-        if local_ip && host == '0.0.0.0'
-          Jekyll.logger.info "  Network Access (Mobile/Other Devices):"
-          Jekyll.logger.info "    http://#{local_ip}:#{port}"
-          Jekyll.logger.info ""
-          Jekyll.logger.info "  📱 Scan this on your mobile browser:"
-          Jekyll.logger.info "    http://#{local_ip}:#{port}"
-          Jekyll.logger.info ""
-          Jekyll.logger.info "  ℹ️  Make sure your mobile device is on the same Wi-Fi network"
-        elsif host == 'localhost' || host == '127.0.0.1'
-          Jekyll.logger.warn "  ⚠️  Network access disabled (host: #{host})"
-          Jekyll.logger.warn "  ℹ️  To enable mobile testing, set 'host: 0.0.0.0' in _config.yml"
-        end
-
-        Jekyll.logger.info ""
-        Jekyll.logger.info "=" * 60
-        Jekyll.logger.info ""
-      end
-    end
+original_logger_info = Jekyll.logger.method(:info)
+Jekyll.logger.define_singleton_method(:info) do |topic, *msg|
+  if topic.to_s.include?("Server address")
+    msg = msg.map { |m| m.to_s.gsub("0.0.0.0", "localhost") }
   end
+  original_logger_info.call(topic, *msg)
 end
 
-# Hook into Jekyll's site generation
 Jekyll::Hooks.register :site, :post_write do |site|
-  # Display network info after first build
-  Jekyll::ServerInfo.display_network_info(site)
+  next unless defined?(Jekyll::Commands::Serve)
+  next if @server_info_displayed
+
+  @server_info_displayed = true
+
+  port    = site.config['port'] || 4000
+  host    = site.config['host'] || 'localhost'
+  baseurl = site.config['baseurl'].to_s
+
+  local_ip = Socket.ip_address_list.detect { |a|
+    a.ipv4? && !a.ipv4_loopback? && !a.ipv4_multicast?
+  }&.ip_address rescue nil
+
+  Jekyll.logger.info ""
+  Jekyll.logger.info "=" * 60
+  Jekyll.logger.info "Network Server Information".center(60)
+  Jekyll.logger.info "=" * 60
+  Jekyll.logger.info ""
+  Jekyll.logger.info "  Local:"
+  Jekyll.logger.info "    http://localhost:#{port}#{baseurl}"
+  Jekyll.logger.info ""
+
+  if local_ip && host == '0.0.0.0'
+    Jekyll.logger.info "  Network:"
+    Jekyll.logger.info "    http://#{local_ip}:#{port}#{baseurl}"
+    Jekyll.logger.info ""
+  elsif host == 'localhost' || host == '127.0.0.1'
+    Jekyll.logger.warn "  Network access disabled (host: #{host})"
+    Jekyll.logger.warn "  Set 'host: 0.0.0.0' in _config.yml for mobile testing"
+    Jekyll.logger.info ""
+  end
+
+  Jekyll.logger.info "=" * 60
+  Jekyll.logger.info ""
 end
